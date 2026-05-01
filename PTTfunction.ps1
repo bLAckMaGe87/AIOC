32a4806980f44f739dec6315c1d81482fa31d5ad
bLAckMaGe87 <130323061+bLAckMaGe87@users.noreply.github.com>
2026-05-01 11:35:49 -0700

    Update PTTfunction.ps1

--- PTTfunction.ps1
+++ PTTfunction.ps1
@@ -5,23 +5,24 @@ $Signature = @'
 public static extern short GetAsyncKeyState(int vKey);
 '@
             $User32 = Add-Type -MemberDefinition $Signature -Name "Win32GetAsyncKeyState" -Namespace Win32Functions -PassThru
-            $KeyToWatch = 0x10 
-            $breakloop = 0 
+            $KeyToWatch = 0x10
+            $KeyToWatch2 = 0x45 
+            $KeyToWatch3 = 0x57
+            $breakloop = 0
             
 
 
             $synth = New-Object System.Speech.Synthesis.SpeechSynthesizer
             $dog = Get-AudioDevice -List | Where-Object { $_.Type -eq "Playback" }
-            $cat = $dog | Where-Object {$_.name-match "AIOC*"} #name your AIOC playback device i.e. "...{$_.name-match "KO6HTC*"}...that's my name.
+             #update with algorithm for COM auto-selector (its in c# right now)
+            $cat = $dog | Where-Object {$_.name-match "AIOC*"} #name your AIOC playback device i.e. "...{$_.name-match "KO6HTCsAIOC_playback*"}
             Set-AudioDevice -Index $cat.index #-Playback
-
-            #update with algorithm for COM auto-selector (its in c# right now)
-            $port = New-Object System.IO.Ports.SerialPort COM5,9600,None,8,one #check your COM number per device
+            $port = New-Object System.IO.Ports.SerialPort COM5,9600,None,8,one #check your COM number per device...
             $port.open()
             Write-Host "PORT OPENING" -BackgroundColor DarkRed -ForegroundColor Blue
             Write-Host "DEFAULT PRESS <Q> to HOLD TX, <SHIFT> TO PTT" -BackgroundColor DarkRed -ForegroundColor Blue
 
-            #$alreadySaidOn = 0
+             
             $alreadySaidOff = 0
             $keyBeingPushed = 0
             $keyAlreadyOn = 0 
@@ -32,6 +33,20 @@ public static extern short GetAsyncKeyState(int vKey);
                 
                 $KeyState = $User32::GetAsyncKeyState($KeyToWatch)
                 $isHeld = [bool]($KeyState -band 0x8000)
+                $KeyState2 = $User32::GetAsyncKeyState($KeyToWatch2)
+                $isHeld2 = [bool]($KeyState2 -band 0x8000)
+                $KeyState3 = $User32::GetAsyncKeyState($KeyToWatch3)
+                $isHeld3 = [bool]($KeyState3 -band 0x8000)
+                if($isHeld2)
+                {
+                    [System.Console]::Beep(1300, 200) #make your own beeps
+
+                }
+                if($isHeld3)
+                {
+                    [System.Console]::Beep(900, 400)
+
+                }
 
                 if($isHeld) 
                 {
@@ -41,6 +56,7 @@ public static extern short GetAsyncKeyState(int vKey);
                         $port.DtrEnable = $true
                         $keyBeingPushed = 1
                         $keyAlreadyOn = 0
+                            
                         }
                    
                 }
